09368de2f0372be22cca7a37f115e10ecebfab56
nachoespino53 <nachoespino53@gmail.com>
2018-10-02 17:23:33 -0400

    More Changes

--- bin/run.rb
+++ bin/run.rb
@@ -2,51 +2,74 @@ require_relative '../config/environment'
 require 'pry'
 
 def run
-
-### Beginning of our program
-  puts "Welcome to our News App"
-  puts "Please tell us your first and last name:"
-  response = gets.chomp
-
-### Now I want to check if it's a valid name, or the input is "exit"
-  return puts "Goodbye" if response.downcase == 'exit'
-  user = nil
   loop do
-    if response.split.count == 2
-      user = store_or_get_name(response)
-      break
-    else
-      puts "Please enter a valid name:"
-      response = gets.chomp
-    end
-  end
-### After the user has been retrieved or created, now give the users some options
-  puts "Welcome #{user.first_name}"
-
-  loop do
-    puts "Please select from the following:"
-    puts "- Saved Articles \n- Topics"
-    response = gets.chomp
-
-    case response.downcase
-    when "saved articles"
-      saved_articles(user)
-    when "topics"
-      topics
+    ### Beginning of our program
+    response = welcome
+    case response
     when 'exit'
       puts "Goodbye"
       break
     else
-      "Invalid Input"
+      ### Now I want to check if it's a valid name
+        user = nil
+        loop do
+          if response.split.count == 2
+            user = store_or_get_name(response)
+            break
+          else
+            puts "Please enter a valid name:"
+            response = gets.chomp
+          end
+        end
+      ### After the user has been retrieved or created, now give the users some options
+        puts "Welcome #{user.first_name}"
+
+        loop do
+          puts "Please select from the following:"
+          puts "- Saved Articles \n- Topics"
+          response = gets.chomp
+
+          case response.downcase
+          when "saved articles"
+            saved_articles(user)
+          when "topics"
+            topics
+          when 'exit'
+            puts "Goodbye"
+            break
+          else
+            "Invalid Input"
+          end
+        end
+      end
     end
-
-  end
-
 end
 
 
 
 ### Methods
+
+def welcome
+  puts "Welcome to our News App"
+  puts "Please tell us your first and last name:"
+  gets.chomp
+end
+
+
+#
+# def valid_name?
+#   return puts "Goodbye" if response.downcase == 'exit'
+#   user = nil
+#   loop do
+#     if response.split.count == 2
+#       user = store_or_get_name(response)
+#       break
+#     else
+#       puts "Please enter a valid name:"
+#       response = gets.chomp
+#     end
+# end
+
 def store_or_get_name(response)
   response = response.split
   user = User.find_by(first_name: response[0], last_name: response[1])
@@ -97,8 +120,5 @@ def topics
     puts "Input invalid"
   end
 end
-
-
-
 
 run
