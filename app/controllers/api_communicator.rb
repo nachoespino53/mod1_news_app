19d4e384556f3798332828ad3c5764139ef81336
suttonau <sutton_mjbc@icloud.com>
2018-10-02 14:08:45 -0400

    Update news.rb

--- app/controllers/api_communicator.rb
+++ app/controllers/api_communicator.rb
@@ -3,9 +3,9 @@ require 'json'
 require 'pry'
 
 
-news = News.new("a9bec172fc834808922ae745dff0d340")
+news_api = News.new("a9bec172fc834808922ae745dff0d340")
 
 # top headlines
 def headlines
-  news.get_top_headlines(sources: "tech-crunch")
+  news_api.get_top_headlines(sources: "tech-crunch")
 end
