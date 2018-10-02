730633a6812e83bd350928c9998f45677f4523dc
nachoespino53 <nachoespino53@gmail.com>
2018-10-02 11:46:42 -0400

    Working on run

--- bin/run.rb
+++ bin/run.rb
@@ -2,24 +2,29 @@ require_relative '../config/environment'
 require 'pry'
 
 def run
-  puts "Welcome, please tell us your name"
-  loop do
+  puts "Welcome to our News App"
+  puts "Please tell us your first and last name:"
   response = gets.chomp
-  names_array = name_array(response)
-  names_array ? (
-    store_or_get_name(names_array)
-    break
-    ) : (puts "Please enter valid name")
+
+  # Now I want to check if it's a valid name, or the input is "exit"
+  return if response.downcase == 'exit'
+  user = nil
+  loop do
+    if response.split.count == 2
+      user = store_or_get_name(response)
+      break
+    else
+      puts "Please enter a valid name:"
+      response = gets.chomp
+    end
   end
 end
 
 
-def name_array(name)
-  name.split.count == 2 ? (return name.split) : (return nil)
-end
-
 def store_or_get_name(response)
-  User.find_by(first_name: response[0], last_name: response[1]) ? (puts "Welcome back") : (User.create(first_name: response[0], last_name: response[1]))
+  response = response.split
+  user = User.find_by(first_name: response[0], last_name: response[1])
+  user ? (return user) : (User.create(first_name: response[0], last_name: response[1]))
 end
 
 run
