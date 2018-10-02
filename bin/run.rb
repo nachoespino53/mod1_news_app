c5ea08408802f266e1336876a3c911f4308da6d5
nachoespino53 <nachoespino53@gmail.com>
2018-10-02 13:46:06 -0400

    Added data to database

--- bin/run.rb
+++ bin/run.rb
@@ -2,11 +2,13 @@ require_relative '../config/environment'
 require 'pry'
 
 def run
+
+### Beginning of our program
   puts "Welcome to our News App"
   puts "Please tell us your first and last name:"
   response = gets.chomp
 
-  # Now I want to check if it's a valid name, or the input is "exit"
+### Now I want to check if it's a valid name, or the input is "exit"
   return puts "Goodbye" if response.downcase == 'exit'
   user = nil
   loop do
@@ -19,12 +21,30 @@ def run
     end
   end
 ### After the user has been retrieved or created, now give the users some options
-puts "Welcome #{user.first_name}"
-puts "Please select from the following:"
-puts "- Saved Articles \n- Topics"
+  puts "Welcome #{user.first_name}"
+  loop do
+    puts "Please select from the following:"
+    puts "- Saved Articles \n- Topics"
+    response = gets.chomp
+    case response.downcase
+
+    when "saved articles"
+      puts "saved article method here"
+    when "topics"
+      puts "topics moethod here"
+    when 'exit'
+      puts "Goodbye"
+      break
+    else
+      "Invalid Input"
+    end
+  end
+
 end
 
 
+
+### Methods
 def store_or_get_name(response)
   response = response.split
   user = User.find_by(first_name: response[0], last_name: response[1])
