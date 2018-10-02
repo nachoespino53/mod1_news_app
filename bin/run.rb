f0e425200a37192965d13c1d63c22b819b4265ff
nachoespino53 <nachoespino53@gmail.com>
2018-10-02 11:18:18 -0400

    Starting the Run

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,25 @@
 require_relative '../config/environment'
+require 'pry'
+
+def run
+  puts "Welcome, please tell us your name"
+  loop do
+  response = gets.chomp
+  names_array = name_array(response)
+  names_array ? (
+    store_or_get_name(names_array)
+    break
+    ) : (puts "Please enter valid name")
+  end
+end
 
 
-puts "Welcome, please tell us your name"
+def name_array(name)
+  name.split.count == 2 ? (return name.split) : (return nil)
+end
+
+def store_or_get_name(response)
+  User.find_by(first_name: response[0], last_name: response[1]) ? (puts "Welcome back") : (User.create(first_name: response[0], last_name: response[1]))
+end
+
+run
