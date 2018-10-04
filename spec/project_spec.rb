f5b6572b685c691b6e3e43af19cae7418cb16ccf
suttonau <sutton_mjbc@icloud.com>
2018-10-04 16:39:40 -0400

    adding rspec files

---
+++ spec/project_spec.rb
@@ -0,0 +1,15 @@
+# require_relative '../config/environment.rb'
+# require_relative '../bin/run'
+
+
+describe '#run' do
+  it "welcomes the user and stores their first and last name" do
+
+    subject.stub(:gets).and_return(response)
+
+  end
+end
+
+# "should return topics if user chooses topics"
+#
+# "should return saved articles if user chooses saved articles"
