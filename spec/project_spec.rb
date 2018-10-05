11d927eb0a5b174342980a740f97218047258998
suttonau <sutton_mjbc@icloud.com>
2018-10-05 10:55:19 -0400

    RSPEC

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,14 +2,23 @@
 # require_relative '../bin/run'
 
 
-describe '#run' do
-  it "welcomes the user and stores their first and last name" do
+describe '#welcome' do
+  #   expect(welcome).to output("\nWelcome to our News App\n\nPlease tell us your first and last name:")
+      it 'receives and returns name as input' do
+        allow($stdin).to receive(:gets).and_return('foo')
+        name = $stdin.gets
 
-    subject.stub(:gets).and_return(response)
-
+        expect(name).to eq('foo')
   end
 end
 
 # "should return topics if user chooses topics"
+
+# describe '#welcome' do
+#       it 'receives and returns name as input' do
+#         allow($stdin).to receive(:gets).and_return('foo')
+#         name = $stdin.gets
 #
+#         expect(name).to eq('foo')
+
 # "should return saved articles if user chooses saved articles"
